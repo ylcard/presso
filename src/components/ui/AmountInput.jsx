54e030a561b9bd75ba39b83f25e311a34f08ea60
Vadim Riskin <vadimgarai@gmail.com>
2025-11-13 18:22:58 +0000

    Manual code change

--- src/components/ui/AmountInput.jsx
+++ src/components/ui/AmountInput.jsx
@@ -105,7 +105,7 @@ export default function AmountInput({
                 className={`${settings.currencyPosition === 'before' ? 'pl-8' : 'pr-8'} ${className}`}
                 {...props}
             />
-            {settings.currencyPosition === 'after' && displayValue !== null && (
+            {settings.currencyPosition === 'after' && (
                 <div className="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
                     <span className="text-gray-500 sm:text-sm font-medium">
                         {displaySymbol}
