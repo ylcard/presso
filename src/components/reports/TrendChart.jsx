deeeebe45cd45087a21f38a30bfcec80de49d290
Vadim Riskin <vadimgarai@gmail.com>
2025-11-21 10:14:25 +0000

    Manual code change

--- src/components/reports/TrendChart.jsx
+++ src/components/reports/TrendChart.jsx
@@ -85,6 +85,7 @@ export default function TrendChart({
                             setSelectedMonth(month);
                             setSelectedYear(year);
                         }}
+                        horizontal={true}
                     />
                 </div>
             </CardHeader>
