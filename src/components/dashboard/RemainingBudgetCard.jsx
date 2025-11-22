e704d36ecddf778711c7e8a39da5d5b7de9e76c5
Vadim Riskin <vadimgarai@gmail.com>
2025-11-22 10:23:05 +0000

    Manual code change

--- src/components/dashboard/RemainingBudgetCard.jsx
+++ src/components/dashboard/RemainingBudgetCard.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Card, CardContent } from "@/components/ui/card";
 import { TrendingUp, TrendingDown } from "lucide-react";
 import { formatCurrency } from "../utils/currencyUtils";
