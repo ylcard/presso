5513967c706e3ac464993251ecb8b39457e0078c
Vadim Riskin <vadimgarai@gmail.com>
2025-11-22 23:49:29 +0000

    Manual code change

--- src/components/transactions/QuickAddTransaction.jsx
+++ src/components/transactions/QuickAddTransaction.jsx
@@ -8,7 +8,7 @@ import {
 import { CustomButton } from "@/components/ui/CustomButton";
 import { Plus } from "lucide-react";
 import { useSettings } from "../utils/SettingsContext";
-import { useCashWallet } from "../hooks/useBase44Entities";
+import { useCashWallet, useAllBudgets } from "../hooks/useBase44Entities";
 import TransactionFormContent from "./TransactionFormContent";
 
 export default function QuickAddTransaction({
@@ -27,6 +27,7 @@ export default function QuickAddTransaction({
 }) {
     const { user } = useSettings();
     const { cashWallet } = useCashWallet(user);
+    const { allBudgets } = useAllBudgets(user);
 
     return (
         <Dialog open={open} onOpenChange={onOpenChange}>
@@ -42,16 +43,18 @@ export default function QuickAddTransaction({
                     </CustomButton>
                 </DialogTrigger>
             )}
-            <DialogContent className="sm:max-w-[500px]">
+            <DialogContent className="sm:max-w-[500px] fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
                 <DialogHeader>
                     <DialogTitle>Quick Add Expense</DialogTitle>
                 </DialogHeader>
                 <TransactionFormContent
                     initialTransaction={defaultCustomBudgetId ? {
+                        amount: null,
                         customBudgetId: defaultCustomBudgetId,
+                        date: allBudgets.find(b => b.id === defaultCustomBudgetId)?.startDate
                     } : null}
                     categories={categories}
-                    allBudgets={customBudgets || []}
+                    allBudgets={allBudgets}
                     onSubmit={onSubmit}
                     onCancel={() => onOpenChange(false)}
                     isSubmitting={isSubmitting}
