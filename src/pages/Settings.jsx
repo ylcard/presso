9a06ce07f788b8b901d0e17b9a00b2a73c54284c
Vadim Riskin <vadimgarai@gmail.com>
2025-11-22 10:16:41 +0000

    Manual code change

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Card, CardContent, CardHeader, CardTitle, CardDescription } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { CustomButton } from "@/components/ui/CustomButton";
