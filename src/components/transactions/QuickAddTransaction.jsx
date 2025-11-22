c3d4a68d5509810a282dd35f37cee0e4951cfab3
Vadim Riskin <vadimgarai@gmail.com>
2025-11-22 10:27:56 +0000

    Manual code change

--- src/components/transactions/QuickAddTransaction.jsx
+++ src/components/transactions/QuickAddTransaction.jsx
@@ -1,78 +1,66 @@
-import React from "react";
 import {
-  Dialog,
-  DialogContent,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
+    Dialog,
+    DialogContent,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
 } from "@/components/ui/dialog";
-// UPDATED 15-Jan-2025: Changed Button import to CustomButton
 import { CustomButton } from "@/components/ui/CustomButton";
 import { Plus } from "lucide-react";
 import { useSettings } from "../utils/SettingsContext";
 import { useCashWallet } from "../hooks/useBase44Entities";
 import TransactionFormContent from "./TransactionFormContent";
 
-export default function QuickAddTransaction({ 
-  open, 
-  onOpenChange, 
-  categories, 
-  customBudgets, 
-  defaultCustomBudgetId = '', 
-  onSubmit, 
-  isSubmitting,
-  transactions = [],
-  renderTrigger = true, // NEW: Control whether to render the trigger button
-  triggerVariant = "default", // NEW: Allow customization of trigger button style
-  triggerSize = "default", // NEW: Allow customization of trigger button size
-  triggerClassName = "" // NEW: Allow custom classes for trigger button
+export default function QuickAddTransaction({
+    open,
+    onOpenChange,
+    categories,
+    customBudgets,
+    defaultCustomBudgetId = '',
+    onSubmit,
+    isSubmitting,
+    transactions = [],
+    renderTrigger = true,
+    triggerVariant = "default",
+    triggerSize = "default",
+    triggerClassName = ""
 }) {
-  const { user } = useSettings();
-  const { cashWallet } = useCashWallet(user);
+    const { user } = useSettings();
+    const { cashWallet } = useCashWallet(user);
 
-  return (
-    <Dialog open={open} onOpenChange={onOpenChange}>
-      {renderTrigger && (
-        <DialogTrigger asChild>
-          {/* UPDATED 15-Jan-2025: Changed to CustomButton */}
-          <CustomButton 
-            variant={triggerVariant}
-            size={triggerSize}
-            className={triggerClassName}
-          >
-            <Plus className="w-4 h-4 mr-2" />
-            Add Expense
-          </CustomButton>
-        </DialogTrigger>
-      )}
-      <DialogContent className="sm:max-w-[500px] fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-        <DialogHeader>
-          <DialogTitle>Quick Add Expense</DialogTitle>
-        </DialogHeader>
-        <TransactionFormContent
-          initialTransaction={defaultCustomBudgetId ? { 
-            amount: null, // <-- FIX: Explicitly set amount to null
-            customBudgetId: defaultCustomBudgetId,
-            date: customBudgets.find(b => b.id === defaultCustomBudgetId)?.startDate
-          } : null}
-          categories={categories}
-          allBudgets={customBudgets}
-          onSubmit={onSubmit}
-          onCancel={() => onOpenChange(false)}
-          isSubmitting={isSubmitting}
-          cashWallet={cashWallet}
-          transactions={transactions}
-        />
-      </DialogContent>
-    </Dialog>
-  );
+    return (
+        <Dialog open={open} onOpenChange={onOpenChange}>
+            {renderTrigger && (
+                <DialogTrigger asChild>
+                    <CustomButton
+                        variant={triggerVariant}
+                        size={triggerSize}
+                        className={triggerClassName}
+                    >
+                        <Plus className="w-4 h-4 mr-2" />
+                        Add Expense
+                    </CustomButton>
+                </DialogTrigger>
+            )}
+            <DialogContent className="sm:max-w-[500px] fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                <DialogHeader>
+                    <DialogTitle>Quick Add Expense</DialogTitle>
+                </DialogHeader>
+                <TransactionFormContent
+                    initialTransaction={defaultCustomBudgetId ? {
+                        amount: null,
+                        customBudgetId: defaultCustomBudgetId,
+                        date: customBudgets.find(b => b.id === defaultCustomBudgetId)?.startDate
+                    } : null}
+                    categories={categories}
+                    allBudgets={customBudgets}
+                    onSubmit={onSubmit}
+                    onCancel={() => onOpenChange(false)}
+                    isSubmitting={isSubmitting}
+                    cashWallet={cashWallet}
+                    transactions={transactions}
+                />
+            </DialogContent>
+        </Dialog>
+    );
 }
-
-// ENHANCEMENT (13-Jan-2025): Added trigger button to QuickAddTransaction component
-// - Now renders its own "Add Expense" button by default
-// - Added renderTrigger prop to control button visibility (for backward compatibility)
-// - Added triggerVariant, triggerSize, and triggerClassName props for button customization
-// - Dialog is now properly centered using fixed positioning (top-1/2 left-1/2 transform)
-// - This fixes the missing "Add Expense" button issue in BudgetDetail page
-// PREVIOUS FIX (2025-01-11): Changed from Popover to Dialog to match QuickAddIncome design
-// UPDATED 15-Jan-2025: Replaced Button with CustomButton for trigger button
