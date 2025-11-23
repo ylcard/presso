c57049e1071c3c2fc6d8073c8755c3d61f4ccb19
Vadim Riskin <vadimgarai@gmail.com>
2025-11-23 20:52:10 +0000

    Manual code change

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -167,7 +167,7 @@ export default function Dashboard() {
                 <div className="grid lg:grid-cols-3 gap-6">
                     <div className="lg:col-span-2 flex flex-col">
                         <BudgetBars
-                            systemBudgets={systemBudgets} 
+                            systemBudgets={systemBudgets}
                             customBudgets={activeCustomBudgets}
                             allCustomBudgets={allCustomBudgets}
                             transactions={transactions}
